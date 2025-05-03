93c6deff566db1ab7dd6750c859c9a13e3f0f242
anupsingh3105 <anup.singh3t@gmail.com>
2025-05-04 01:45:50 +0530

    New code

--- allZerosOnLeft.java
+++ allZerosOnLeft.java
@@ -38,4 +38,17 @@ public class allZerosOnLeft {
         }
         System.out.println(Arrays.toString(arr2));
     }
+
+    // public static void main(String[] args) {
+    // int[] arr = { 0, 8, 2, 4, 0, 0, 6, 0, 0, 7, 0 };
+    // int endIndex = arr.length - 1;
+    // for (int i = arr.length - 1; i >= 0; i--) {
+    // if (arr[i] != 0) {
+    // arr[endIndex] = arr[i];
+    // endIndex--;
+    // arr[i] = 0;
+    // }
+    // }
+    // System.out.println(Arrays.toString(arr));
+    // }
 }
